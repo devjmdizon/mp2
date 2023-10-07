ea273a456357f35d5119228058bbb3481f128cf3
Joseph Michael Dizon <dizonjoseph23@gmail.com>
2023-10-07 09:45:57 +0800

    Update changeBackground contents for images

--- src/changeBackground.js
+++ src/changeBackground.js
@@ -4,22 +4,22 @@ export default async function changeBackground(time) {
   if (time >= "00:00:00" && time < "05:00:00") {
     changeGreeting(time);
     return (document.body.style.backgroundImage =
-      'url("./images/midnight-image.jpg")');
+      'url("../images/midnight-image.jpg")');
   } else if (time >= "05:00:00" && time < "12:00:00") {
     changeGreeting(time);
     return (document.body.style.backgroundImage =
-      'url("./images/morning-image.jpeg")');
+      'url("../images/morning-image.jpeg")');
   } else if (time >= "12:00:00" && time < "13:00:00") {
     changeGreeting(time);
     return (document.body.style.backgroundImage =
-      'url("./images/lunch-image.png")');
+      'url("../images/lunch-image.png")');
   } else if (time > "13:00:00" && time < "17:00:00") {
     changeGreeting(time);
     return (document.body.style.backgroundImage =
-      'url("./images/afternoon-image.png")');
+      'url("../images/afternoon-image.png")');
   } else {
     changeGreeting(time);
     return (document.body.style.backgroundImage =
-      'url("./images/night-image.jpg")');
+      'url("../images/night-image.jpg")');
   }
 }
