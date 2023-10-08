d9cd480fbbfb30a5f4282ce0d5a83b7dca9ab76b
Joseph Michael Dizon <dizonjoseph23@gmail.com>
2023-10-08 21:58:47 +0800

    Update changeBackground contents

--- src/changeBackground.js
+++ src/changeBackground.js
@@ -1,31 +1,25 @@
 import changeGreeting from "./changeGreeting";
 
 export default async function changeBackground(time) {
-  try {
+  if (time >= "00:00:00" && time < "05:00:00") {
     changeGreeting(time);
-
-    let backgroundImageUrl;
-    if (time >= "00:00:00" && time < "05:00:00") {
-      backgroundImageUrl = "./src/asset/midnight-image.jpg";
-    } else if (time >= "05:00:00" && time < "12:00:00") {
-      backgroundImageUrl = "./src/asset/morning-image.jpeg";
-    } else if (time >= "12:00:00" && time < "13:00:00") {
-      backgroundImageUrl = "./src/asset/lunch-image.png";
-    } else if (time > "13:00:00" && time < "17:00:00") {
-      backgroundImageUrl = "./src/asset/afternoon-image.png";
-    } else {
-      backgroundImageUrl = "./src/asset/night-image.jpg";
-    }
-
-    const image = new Image();
-    image.src = backgroundImageUrl;
-    image.onload = function () {
-      document.body.style.background = `url(${backgroundImageUrl})`;
-    };
-    image.onerror = function () {
-      console.error("Error loading background image:", backgroundImageUrl);
-    };
-  } catch (error) {
-    console.error("Error changing background:", error);
+    return (document.body.style.backgroundImage =
+      'url("https://images.hdqwalls.com/wallpapers/midnight-sunrise-4k-b1.jpg")');
+  } else if (time >= "05:00:00" && time < "12:00:00") {
+    changeGreeting(time);
+    return (document.body.style.backgroundImage =
+      'url("https://wallpapercave.com/wp/wp10712591.jpg")');
+  } else if (time >= "12:00:00" && time < "13:00:00") {
+    changeGreeting(time);
+    return (document.body.style.backgroundImage =
+      'url("https://img.gltuae.com/ContentImages/KOREA-JAPAN-0-2048x1217.jpeg")');
+  } else if (time > "13:00:00" && time < "17:00:00") {
+    changeGreeting(time);
+    return (document.body.style.backgroundImage =
+      'url("https://wallpapercave.com/wp/wp9123824.jpg")');
+  } else {
+    changeGreeting(time);
+    return (document.body.style.backgroundImage =
+      'url("https://wallpapercave.com/wp/wp2737727.jpg")');
   }
 }
